f44c56cb8132080dcdc43fc5a92009196a9ae669
gumerova.e <gumerova.e@etton.ru>
2021-11-10 17:06:44 +0300

    fix: deleting files 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,16 +5,16 @@ const copyFolderPath = path.join(__dirname, 'files-copy');
 const sourceFolderPath = path.join(__dirname, 'files');
 
 const copyDir = () => {
-  fs.mkdir(copyFolderPath, { recursive: true }, err => {
-    if(err) console.log('Error', err.message);
+  fs.mkdir(copyFolderPath, {recursive: true}, err => {
+    if(err) console.log('Directory hasn\'t been maked', err.message);
 
     fs.readdir(sourceFolderPath, (err, data) => {
       if(err) console.log('Error', err.message);
-
+  
       data.forEach(file => {
         const sourceFile = path.join(__dirname, 'files', file);
         const copyFile = path.join(__dirname, 'files-copy', file);
-
+  
         fs.copyFile(sourceFile, copyFile, err => {
           if(err) throw err;
         });
@@ -23,4 +23,18 @@ const copyDir = () => {
   });
 };
 
-copyDir();
+
+
+
+fs.access(copyFolderPath, err => {
+  if(err) {
+    copyDir();
+  } else {
+    console.log('Directory exists');
+
+    fs.rm(copyFolderPath, { recursive: true }, (err) => {
+      if(err) console.log('Directory didn\'t delete', err.message);
+      copyDir();
+    });
+  }
+});
